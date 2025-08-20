98a0bc1dbe1f1a1639a008d431aeac45e2513e9f
Muhamamd Tayyab <122749509+codewithtayyab1@users.noreply.github.com>
2025-08-20 11:51:46 +0500

    "Added variables program in C++"

---
+++ variables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Different types of variables
+    int age = 20;
+    float gpa = 3.5;
+    char grade = 'A';
+    bool isStudent = true;
+    string name = "Tayyab";
+
+    // Print values
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "GPA: " << gpa << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << isStudent << endl;
+
+    return 0;
+}
